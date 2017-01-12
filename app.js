e9cd44a615472eae20b2724d4106795d78d87320
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-12 12:22:43 +0200

    omitting the ::ffff: IPv6 part from the ip.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require ('express'),
       favicon = require ('serve-favicon'),
       app = express ()
 
+app.enable('trust proxy')
 app.use (favicon ((__dirname + '/public/favicon.ico')))
 app.get ('/', (req, res) => {
     let userAgent = req.get('user-agent'),
-        ip = req.ip,
+        ip = req.ip.match (/[\d.]/)[0],
         lang = req.get ('accept-language').split (',')[0]
+//    console.log (ip)
     platform = userAgent.match (/[\w\s;.]*(?=\))/)[0]
     res.json ({
         ip: ip,
